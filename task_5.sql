8a144f23cbecf8bb238eb1f1e597c9038440ffc4
GTA__Civilian <karabomunano@gmail.com>
2025-02-03 03:08:08 -0800

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,4 @@
 USE alx_book_store;
 INSERT INTO customers (Customer_ID, FirstName, LastName, Email, Phone, Address, City, Country)
+  customer_id, customer_name, email, address
 VALUES (1, 'Cole', 'Baidoo', 'cbaidoo@sandtech.com', '', '123 Happiness Ave.', '', '');

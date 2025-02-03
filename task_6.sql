a73fbd5f81465e14e49cb4596e86c3ac94a763bb
GTA__Civilian <karabomunano@gmail.com>
2025-02-03 03:13:06 -0800

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -3,3 +3,5 @@ VALUES
   (2, 'Blessing', 'Malik', 'bmalik@sandtech.com', '', '124 Happiness Ave.', '', ''),
   (3, 'Obed', 'Ehoneah', 'eobed@sandtech.com', '', '125 Happiness Ave.', '', ''),
   (4, 'Nehemial', 'Kamolu', 'nkamolu@sandtech.com', '', '126 Happiness Ave.', '', '');
+
+Blessing Malik, 124 Happiness  Ave.

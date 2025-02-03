72a899bdca25d78097376605f87a9fb19e1b8078
GTA__Civilian <karabomunano@gmail.com>
2025-02-03 03:06:42 -0800

    Create task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,3 @@
+USE alx_book_store;
+INSERT INTO customers (Customer_ID, FirstName, LastName, Email, Phone, Address, City, Country)
+VALUES (1, 'Cole', 'Baidoo', 'cbaidoo@sandtech.com', '', '123 Happiness Ave.', '', '');
